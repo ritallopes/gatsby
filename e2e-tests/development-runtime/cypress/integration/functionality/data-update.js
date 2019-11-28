db74432cb411b1dd67a76882c6be7aab13ab8e0d
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2019-11-28 22:03:04 +0700

    Add e2e test for schema rebuilding in develop

--- e2e-tests/development-runtime/cypress/integration/functionality/data-update.js
+++ e2e-tests/development-runtime/cypress/integration/functionality/data-update.js
@@ -2,30 +2,46 @@ const FILE_CONTENT = `
 ---
 title: A new post
 date: ${new Date().toJSON()}
+# foo: freshField
 ---
 
 A brand new post
 
 `.trim()
 
+const updateSampleFileCommand = replacements => {
+  const content = JSON.stringify(FILE_CONTENT)
+  let command = `npm run update -- --file content/sample.md --file-content \\"${content}\\"`
+  if (replacements) {
+    command = `${command} --exact --replacements "${replacements}"`
+  }
+  cy.exec(command)
+}
+
 describe(`on new file`, () => {
   beforeEach(() => {
     cy.visit(`/`).waitForRouteChange()
   })
 
-  /*
-   * TODO: This seems to cause a page re-load
-   */
-  it.skip(`re-runs GraphQL queries with new file contents`, () => {
-    cy.exec(
-      `npm run update -- --file content/sample.md --file-content '${JSON.stringify(
-        FILE_CONTENT
-      )}'`
-    )
+  it(`re-runs GraphQL queries with new file contents`, () => {
+    updateSampleFileCommand()
 
     cy.get(`ul`)
-      .find(`li`)
-      .its(`length`)
-      .should(`be.gt`, 1)
+      .find(`li:nth-child(2)`)
+      .should(`exist`, 1)
+  })
+})
+
+describe(`on schema change`, () => {
+  beforeEach(() => {
+    updateSampleFileCommand()
+    cy.visit(`/`).waitForRouteChange()
+  })
+
+  it(`rebuilds GraphQL schema`, () => {
+    updateSampleFileCommand(`# foo:foo`)
+
+    cy.visit(`/schema-rebuild/`)
+    cy.get(`p`).contains(`"foo":"freshField"`)
   })
 })
